312af602f5ba693dfeadf2e217705d6ebb142581
willttxp <willimvitolimaamorim13@gmail.com>
2022-03-07 18:57:46 -0300

    codigo completo

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function xuxu(){
+    let usuario= prompt("nome do usuario:")
+let senha= prompt("diga uma senha:")
+if( usuario == "william18" && senha == "123456" ){
+    alert ("senha certo")
+    document.write(" <h1>oi pessoa</h1>")
+}
+else{ alert("senha incorreta")
+
+}
+
+}
